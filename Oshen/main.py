307a867bd8df02dc91158457f4cde899605fd739
osheng7 <oshengeenath2002@gmail.com>
2023-02-04 13:10:36 +0530

    Intrusion detection

--- Oshen/main.py
+++ Oshen/main.py
@@ -1,5 +1,7 @@
 import cv2
-
+import winsound
+freq=1000
+dur=100
 #img = cv2.imread('lena.png')
 thres = 0.5 # Threshold to detect objects
 
@@ -11,6 +13,8 @@ classNames= []
 classFile = 'coco.names'
 with open(classFile,'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
+    print(classNames)
+
 
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 weightsPath = 'frozen_inference_graph.pb'
@@ -29,10 +33,15 @@ while True:
     if len(classIds) != 0:
         for classId, confience,box in zip(classIds.flatten(),confs.flatten(),bbox):
             cv2.rectangle(img,box,color=(0,0,255),thickness=2)
+            if classId == [1]:
+                winsound.Beep(freq, dur)
             cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
                         cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
             cv2.putText(img,str(round(confience*100,2)),(box[0]+200,box[1]+30),
                         cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
 
-    cv2.imshow("Output",img)
+
+
+
+    cv2.imshow("Output", img)
     cv2.waitKey(1)
