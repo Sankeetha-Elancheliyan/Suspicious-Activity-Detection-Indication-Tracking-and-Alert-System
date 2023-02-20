524280d0a0752fb7b373b4c5d351460dd81bf6ba
osheng7 <oshengeenath2002@gmail.com>
2023-02-20 11:51:11 +0530

    Adding the beep sound and making to save the video

--- Oshen/main.py
+++ Oshen/main.py
@@ -1,11 +1,23 @@
 import cv2
 import winsound
+import datetime
 freq=1000
 dur=100
 #img = cv2.imread('lena.png')
 thres = 0.5 # Threshold to detect objects
 
 cap = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+now = datetime.datetime.now()
+date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+output_file = f"output_{date_time}.mp4"
+
+
+out = cv2.VideoWriter(output_file,fourcc,5.0,(640, 480))
+
+
 cap.set(3,640)
 cap.set(4,480)
 
@@ -13,7 +25,6 @@ classNames= []
 classFile = 'coco.names'
 with open(classFile,'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
-    print(classNames)
 
 
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
@@ -34,14 +45,13 @@ while True:
         for classId, confience,box in zip(classIds.flatten(),confs.flatten(),bbox):
             cv2.rectangle(img,box,color=(0,0,255),thickness=2)
             if classId == [1]:
+                out.write(img)
                 winsound.Beep(freq, dur)
+
             cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
                         cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
             cv2.putText(img,str(round(confience*100,2)),(box[0]+200,box[1]+30),
                         cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
-
-
-
 
     cv2.imshow("Output", img)
     cv2.waitKey(1)
